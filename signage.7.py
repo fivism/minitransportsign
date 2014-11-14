f15804b6d7ea3e1e1f9ec41739c1c1b35451e0b6
Michael Fivis <michael.fivis@gmail.com>
2014-11-14 20:08:15 +0100

    r

--- signage.7.py
+++ signage.7.py
@@ -19,31 +19,28 @@ from time import sleep
 ## Initialize serial target
 ser = serial.Serial('/dev/ttyACM0', 115200)
 
-# Pull JSON file for the StopID# 3010131 (12 Trikk)
-try:
-	turl = "http://reis.trafikanten.no/reisrest/realtime/getrealtimedata/3010131"
-	tdata = json.load(urllib2.urlopen(turl))
-	j = 0
-	sentrumtimes = list()
-	majortimes = list()
-	while j < len(tdata):
-		if tdata[j][u'DirectionRef'] == '1':
-			sentrumtimes.append(tdata[j][u'ExpectedArrivalTime'])
-		elif tdata[j][u'DirectionRef'] == '2':
-			majortimes.append(tdata[j][u'ExpectedArrivalTime'])
-		j = j + 1
-
-except:
-	ser.write(b'\xFE')
-	ser.write(b'\x58')
-	ser.write("Connect Error")
-	sys.exit()
-	
 
 # Collect current time
-current = datetime.datetime.fromtimestamp(int(time.time())) ## Current time
 
-for x in range(0, 3):
+while(1):
+	try:
+		turl = "http://reis.trafikanten.no/reisrest/realtime/getrealtimedata/3010531"
+		tdata = json.load(urllib2.urlopen(turl))
+		j = 0
+		sentrumtimes = list()
+		while j < len(tdata):
+			if (tdata[j][u'DirectionRef'] == '2' and tdata[j][u'LineRef'] == '17'):
+				sentrumtimes.append(tdata[j][u'ExpectedArrivalTime'])
+
+			j = j + 1
+
+	except:
+		ser.write(b'\xFE')
+		ser.write(b'\x58')
+		ser.write("Connect Error")
+		#sys.exit()
+	
+	current = datetime.datetime.fromtimestamp(int(time.time())) ## Current time
 
 	## Wipe and Start First Line -- with catchall exception
 	try:
@@ -51,11 +48,11 @@ for x in range(0, 3):
 		stfirst = dateutil.relativedelta.relativedelta (st1, current) ## First diff
 		ser.write(b'\xFE')
 		ser.write(b'\x58')
-		ser.write("Sentrum:    %dm" % stfirst.minutes)
+		ser.write("17 Sentrum:  %dm" % stfirst.minutes)
 	except: 
 		ser.write(b'\xFE')
 		ser.write(b'\x58')
-		ser.write("Sentrum:   NA")
+		ser.write("17 Sentrum:   NA")
 
 
 	## Second Line -- with late night N/A exception
@@ -66,45 +63,16 @@ for x in range(0, 3):
 		ser.write(b'\x47')
 		ser.write("\x01")
 		ser.write("\x02")
-		ser.write("Sentrum:    %dm" % stsecond.minutes)
+		ser.write("17 Sentrum:  %dm" % stsecond.minutes)
 	except:
 		ser.write(b'\xFE')
 		ser.write(b'\x47')
 		ser.write("\x01")
 		ser.write("\x02")
-		ser.write("Sentrum:   NA")
+		ser.write("17 Sentrum:   NA")
 
+	time.sleep(15)
 
-	## Wait (Use with "watch -n 7" bash script)
-	time.sleep(5)
-
-
-	## Wipe and Start First Line -- with catch-all exception
-	try:
-		mt1 = datetime.datetime.fromtimestamp(int(majortimes[0][6:16])) ## First majorstuen time tuple
-		mtfirst = dateutil.relativedelta.relativedelta (mt1, current) ## First diff
-		ser.write(b'\xFE')
-		ser.write(b'\x58')
-		ser.write("Majorstuen: %dm" % mtfirst.minutes)
-	except:
-		ser.write(b'\xFE')
-		ser.write(b'\x58')
-		ser.write("Majorstuen:   NA")
-
-	## Second Line -- with late night N/A exception
-	try:
-		mt2 = datetime.datetime.fromtimestamp(int(majortimes[1][6:16])) ## Second majorstuen time tuple
-		mtsecond = dateutil.relativedelta.relativedelta (mt2, current) ## Second diff
-		ser.write(b'\xFE')
-		ser.write(b'\x47')
-		ser.write("\x01")
-		ser.write("\x02")
-		ser.write("Majorstuen: %dm" % mtsecond.minutes)
-	except:
-		ser.write(b'\xFE')
-		ser.write(b'\x47')
-		ser.write("\x01")
-		ser.write("\x02")
-		ser.write("Majorstuen:  NA")
+## Wait (Use with "watch -n 7" bash script)
 
 
