4a577c44bd9279df5ac2d093032a02efc94d6d4a
fivism <michael.fivis@gmail.com>
2018-08-17 17:53:02 +0200

    modifying error msg

--- signageFunction.py
+++ signageFunction.py
@@ -13,8 +13,8 @@ from time import sleep
 import sys
 import requests # https://gist.github.com/gbaman/b3137e18c739e0cf98539bf4ec4366ad
 
-LCD_ON = True  # for testing with/without serial LCD connection
-DEBUG = False  # set extra output on
+LCD_ON = False  # for testing with/without serial LCD connection
+DEBUG = True  # set extra output on
 PROFILE_FILE = "profiles.txt" # config file with ET_CLIENT_NAME and stations
 
 def prof_reader(filename):
@@ -193,7 +193,9 @@ def mainloop():
         print("ERROR CODE:", sys.exc_info()[0])
         if LCD_ON:
             lcdscreen.clear()
-            lcdscreen.write("VALERROR")
+            lcdscreen.write("VAL Error:")
+            lcdscreen.set_cursor_position(1, 2)
+            lcdscreen.write(sys.exc_info()[0])
         time.sleep(10)
 
     else:
